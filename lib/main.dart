0826cfd1d01a93ac9f21b32bfb09d017a1d3057a
kandelmilan <kandelr950p@gmail.com>
2026-05-20 22:44:54 +0545

    Day 5:Login,GetX and Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:hello_world/views/home_view.dart';
 import 'package:hello_world/views/login_view.dart';
+import 'package:hello_world/views/register_view.dart';
+import 'package:hello_world/views/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +15,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(title: 'Flutter Demo', home: LoginView());
+    return GetMaterialApp(title: 'Flutter Demo', home: HomeView());
   }
 }
